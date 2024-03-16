78aa72cb887b577f710e7c246574a03994e78faf
Ali Feruz <147985313+AliFeruz@users.noreply.github.com>
2024-03-16 13:26:44 +0100

    first commit

---
+++ src/components/Button.tsx
@@ -0,0 +1,34 @@
+import { ReactNode } from "react";
+import ButtonSvg from "../assets/svg/ButtonSvg";
+
+type Props = {
+    children?: ReactNode;
+    className?: string;
+    href?: string;
+    onClick?: () => void;
+    white?: string;
+    px?: string
+}
+
+const Button = ({children, className, href, onClick, white, px}: Props) => {
+    const classes = `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${px || 'px-7'} ${white ? 'text-n-8' : 'text-n-1'} ${className || ''}`
+    const spanClasses = 'relative z-10'
+    if (href) {
+        return (
+            <a href={href} className={classes} onClick={onClick}>
+                <span className={spanClasses}>{children}</span>
+                {ButtonSvg(white!)}
+            </a>
+        );
+    }
+
+ const renderBtn = () => (
+    <button className={classes} onClick={onClick}>
+        <span className={spanClasses}>{children}</span>
+        {ButtonSvg(white!)}
+    </button>
+ )
+ return renderBtn()
+};
+
+export default Button;
