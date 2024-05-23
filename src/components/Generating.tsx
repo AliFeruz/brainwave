7178d18fc79610eb7083060337c382552eab5a01
Ali Feruz <aliferuzcode@gmail.com>
2024-05-23 23:23:12 +0200

    added services section

--- src/components/Generating.tsx
+++ src/components/Generating.tsx
@@ -4,9 +4,9 @@ type Props = {
     className: string;
 }
 
-const Generating = (className: Props) => {
+const Generating = ({className}: Props) => {
   return (
-    <div className={`${className || ''} flex items-center text-base h-[3.5rem] px-6 bg-n-8/80 rounded-[1.7rem]`}>
+    <div className={`${className} flex items-center text-base h-[3.5rem] px-6 bg-n-8/80 rounded-[1.7rem]`}>
     <img src={loading} alt="loading" className='w-5 h-5 mr-4' />
     AI is generating
     </div>
